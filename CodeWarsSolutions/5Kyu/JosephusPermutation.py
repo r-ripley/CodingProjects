663bfb1096e1d5fe05ecc8dd8842f7995bd1df4e
r-ripley <ryan.ripley.b@gmail.com>
2024-05-16 10:03:32 -0400

    working on test cases for Joesphus Permutation

---
+++ CodeWarsSolutions/5Kyu/JosephusPermutation.py
@@ -0,0 +1,16 @@
+def josephus(items, k) -> list:
+    arr = []
+    runs = len(items)
+    spot = k - 1
+    while items:
+        while spot >= len(items):
+            spot -= len(items)
+        if len(items) == 1:
+            arr.append(items[0])
+            items.pop(0)
+            break   
+        arr.append(items[spot])
+        items.pop(spot)
+        spot += k-1
+
+    return arr
