01a6f0ac2a7cdf0c3ca9dd9f4fbd2585fb079f65
r-ripley <ryan.ripley.b@gmail.com>
2024-05-21 08:36:51 -0400

    adding a 4 kyu solution

---
+++ CodeWarsSolutions/fourKyu/HumanReadableDuration.py
@@ -0,0 +1,48 @@
+def format_duration(time):
+    # 3600 seconds in an hour
+    # 86400 seconds in a day
+    # 31536000 seconds in a year
+    
+    timeStr = ""
+    count = 0
+
+    if time == 0:
+        return "now"
+    else:
+        years = time // 31536000
+        days = (time - years * 31536000) // 86400
+        hours = (time - years * 31536000 - days * 86400) // 3600
+        minutes = (time - years * 31536000 - days * 86400 - hours * 3600) // 60
+        seconds = (time - years * 31536000 - days * 86400 - hours * 3600 - minutes * 60)
+        
+        timeDict = {"year": years, 
+                    "day": days, 
+                    "hour": hours, 
+                    "minute": minutes, 
+                    "second": seconds
+                    }
+        
+    def plural(time: str, value: int) -> str:
+        if value > 1:
+            return f'{value} ' + time + 's'
+        elif value == 1:
+            return f'{value} ' + time
+        
+    timeTracker = []
+    for item in timeDict:
+        if timeDict[item] > 0:
+            timeTracker.append([item, timeDict[item]])
+            
+    for item in timeTracker:
+        count += 1
+        if len(timeTracker) == 1:
+            return plural(item[0], item[1])
+        if count == len(timeTracker):
+            timeStr = timeStr + " and " + plural(item[0], item[1])
+        elif count == len(timeTracker) - 1:
+            timeStr = timeStr + plural(item[0], item[1])
+        else:
+            timeStr = timeStr + plural(item[0], item[1]) + ', '
+            
+
+    return timeStr
