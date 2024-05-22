ab454bc46d14201bac8196749fa4088568cff770
r-ripley <ryan.ripley.b@gmail.com>
2024-05-22 14:17:44 -0400

    adding another 4 kyu solution, getting better every day

---
+++ CodeWarsSolutions/fourKyu/CodeWarsStyleRankingSystem.py
@@ -0,0 +1,32 @@
+class User:
+    def __init__(self):
+        self.rank = -8
+        self.progress = 0
+        
+    def rank():
+        return user.rank
+    
+    def progress():
+        return user.progress
+    
+    def inc_progress(self, value):
+        if value == 0 or value > 8 or value < -8:
+            raise Exception("Not a value")
+        else:
+            if self.rank == 8:
+                self.progress = 0
+            elif self.rank == value:
+                self.progress += 3
+            elif self.rank - value == 1 or self.rank == 1 and value == -1:
+                self.progress += 1
+            elif self.rank < 0 and value >= 1:
+                self.progress += 10 * (self.rank - value + 1) * (self.rank - value + 1)
+            elif self.rank < value and (value <= -1 or value >= 1):
+                self.progress += 10 * (self.rank - value) * (self.rank - value)
+            while self.progress >= 100:
+                self.rank += 1
+                self.progress -= 100
+                if self.rank == 0:
+                    self.rank = 1
+                if self.rank == 8:
+                    self.progress = 0
