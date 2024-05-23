f91e1e72ebd2e7294ffe9e4cadbe63779fccafd5
r-ripley <ryan.ripley.b@gmail.com>
2024-05-23 14:00:38 -0400

    forgot the solution for this one with the previous commit

---
+++ CodeWarsSolutions/sixKyu/CountTheSmileyFaces.py
@@ -0,0 +1,13 @@
+def count_smileys(arr):
+    count = 0
+    
+    for item in arr:
+        if len(item) == 2:
+            if (item[0] == ':' or item[0] == ';') and (item[1] == ')' or item[1] == 'D'):
+                count += 1
+                
+        elif len(item) == 3:
+            if (item[0] == ':' or item[0] == ';') and (item[1] == '-' or item[1] == '~') and (item[2] == ')' or item[2] == 'D'):
+                count += 1
+            
+    return count
