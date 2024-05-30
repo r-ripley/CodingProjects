4c53122a4e9cc6b040f34de5f96a92b3f3e3d3e4
r-ripley <ryan.ripley.b@gmail.com>
2024-05-30 08:57:20 -0400

    removed comments after figuring out code logic

--- Grind75Solutions/Easy/MergeTwoSortedLists.py
+++ Grind75Solutions/Easy/MergeTwoSortedLists.py
@@ -1,10 +1,10 @@
 def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # this works but I don't understand why, whats the point of x = y = z
+        
         merged = dummy = ListNode()
         while list1 and list2:
             if list1.val < list2.val:
                 merged.next = list1
-                # I don't know what is going on here, but this works, I thought x, y = a, b is the same as doing x = a y = b
+                
                 list1, merged = list1.next, list1
             else:
                 merged.next = list2
